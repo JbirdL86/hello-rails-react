2a990592d72f23f772039a33329bc7252649a110
JbirdL86 <jgonzalez.wj@gmail.com>
2022-01-21 14:25:29 -0600

    Add routes for things controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-Rails.application.routes.draw do  
+Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
 
-  get '*page', to: 'static#index', constraints: ->(req) do
+  get '*page', to: 'static#index', constraints: lambda { |req|
     !req.xhr? && req.format.html?
-  end
+  }
 
   root 'static#index'
 end
